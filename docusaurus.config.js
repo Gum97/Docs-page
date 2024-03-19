82488d9e7c555ddf7b1a2d9f28e26b178011fb54
Gum97 <duynguyenvt97@gmail.com>
2024-03-19 13:09:23 +0700

    add sitemaps

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,12 +33,12 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
-      },
-      sitemap: {
-        changefreq: 'weekly',
-        priority: 0.5,
-        ignorePatterns: ['/tags/**'],
-        filename: 'sitemap.xml',
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+        },
       },
     ],
   ],
